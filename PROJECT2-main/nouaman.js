f2753bf86e2cabdfbb401f44446152cf802ff125
NouamanGuendoul <113364960+NouamanGuendoul@users.noreply.github.com>
2023-01-13 09:39:06 +0100

    Add files via upload

--- PROJECT2-main/nouaman.js
+++ PROJECT2-main/nouaman.js
@@ -10,8 +10,9 @@ function plus() {
  if ( cliks > 50 ) {
      cliks = cliks + 1;
 } 
-if(cliks > 200){
+if(cliks > 200  && cliks < 201){
      cliks = cliks + 5 - 2 ;
+     alert("geweldig je hebt 200 gehaald")
  }
 
 
@@ -21,7 +22,8 @@ document.getElementById("teller").innerText =   cliks;
 
 function reset(){
      if (cliks = 100) {
-          cliks = 0;         
+          cliks = 0;  
+                 
      } 
      document.getElementById("teller").innerText =   cliks;
 };
@@ -29,6 +31,11 @@ function reset(){
 function keer(){
    
      cliks = cliks * 2;
+     if(cliks > 100000000  && cliks < 200000000 ){
+          alert("je hebt 100000000 gehaald ")
+          
+     }
+   
 
      document.getElementById("teller").innerText =   cliks;
 };
